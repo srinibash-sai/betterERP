44d3f23723ec21a420469fc14d47f76a91fc4e62
Srinivas Sai <srinibash.sai26@gmail.com>
2024-08-30 11:13:31 +0530

    removed time duration for scheduler

--- src/main/java/com/anup/betterERP/scheduler/HealthCheckTask.java
+++ src/main/java/com/anup/betterERP/scheduler/HealthCheckTask.java
@@ -13,27 +13,37 @@ public class HealthCheckTask {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @Scheduled(fixedRate = 840000) // Task runs every 13 minutes (13 * 60 * 1000 ms)
+//    @Scheduled(fixedRate = 840000) // Task runs every 13 minutes (13 * 60 * 1000 ms)
+//    public void sendHealthCheckRequest() {
+//        // Set the time zone to IST
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+//
+//        LocalTime currentTime = LocalTime.now();
+//
+//        // Define the start and end times
+//        LocalTime startTime = LocalTime.of(5, 0);  // 4 AM
+//        LocalTime endTime = LocalTime.of(2, 0);    // 2 AM (next day)
+//
+//        // Check if the current time is within the desired range
+//        if (currentTime.isAfter(startTime) || currentTime.isBefore(endTime)) {
+//            // Send the GET request
+//            String url = "https://bettererp.onrender.com/api/health";
+//            try {
+//                ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+////                System.out.println("Health check response: " + response.getStatusCode());
+//            } catch (Exception e) {
+//                System.err.println("Failed to send health check request: " + e.getMessage());
+//            }
+//        }
+//    }
+
+    @Scheduled(fixedRate = 840000)
     public void sendHealthCheckRequest() {
-        // Set the time zone to IST
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
-
-        LocalTime currentTime = LocalTime.now();
-
-        // Define the start and end times
-        LocalTime startTime = LocalTime.of(5, 0);  // 4 AM
-        LocalTime endTime = LocalTime.of(2, 0);    // 2 AM (next day)
-
-        // Check if the current time is within the desired range
-        if (currentTime.isAfter(startTime) || currentTime.isBefore(endTime)) {
-            // Send the GET request
-            String url = "https://bettererp.onrender.com/api/health";
-            try {
-                ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-//                System.out.println("Health check response: " + response.getStatusCode());
-            } catch (Exception e) {
-                System.err.println("Failed to send health check request: " + e.getMessage());
-            }
+        String url = "https://bettererp.onrender.com/api/health";
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        } catch (Exception e) {
+            System.err.println("Failed to send health check request: " + e.getMessage());
         }
     }
 }
